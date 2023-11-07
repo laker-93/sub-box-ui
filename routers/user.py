af9bd4722142350f6889277f93c28acddc9fd636
luke-1993 <luke_purnell@hotmail.com>
2023-11-07 08:58:06 +0000

    start integrating with pymix. TODO complete error handling when creating user

--- routers/user.py
+++ routers/user.py
@@ -1,3 +1,7 @@
+import aiohttp
+import logging
+from http import HTTPStatus
+
 from containers import Container
 from typing import Optional
 from dependency_injector.wiring import Provide, inject
@@ -7,6 +11,8 @@ from fastapi.templating import Jinja2Templates
 
 
 router = APIRouter()
+
+logger = logging.getLogger(__name__)
 @router.get("/user/signupform", response_class=HTMLResponse)
 async def signup_form(request: Request, hx_request: Optional[str] = Header(None)):
     templates = Jinja2Templates(directory="ui/templates")
@@ -48,12 +54,36 @@ async def create(
     port = config['py_mix']['port']
     print(f'host {host} port {port}')
     print(f'username {username} password {password} email {email}')
+    data = {
+        'username': username,
+        'password': password
+    }
+    error = {}
+    async with aiohttp.ClientSession() as session:
+        async with session.post('http://0.0.0.0:8002/create_user', params=data) as response:
+            error['status_code'] = response.status
+            if response.status == HTTPStatus.OK:
+                response_json = await response.json()
+                print(response_json)
+                if not response_json.get('success'):
+                    error['response'] = response_json.get('reason')
+                    template = 'partials/failure.html'
+                else:
+                    template = 'partials/success.html'
+            else:
+                print(f'error {response.status} {HTTPStatus.OK}')
+                try:
+                    response_json = await response.json()
+                except Exception:
+                    logger.error(f'failed to decode response {response}', exc_info=True)
+                    response_json = ""
+                template = 'partials/failure.html'
+                error['response'] = response_json
+
     templates = Jinja2Templates(directory="ui/templates")
 
-    context = {"request": request, "value": 0}
-    import time
-    time.sleep(1)
-    return templates.TemplateResponse("partials/success.html", context)
+    context = {"request": request, "error": error}
+    return templates.TemplateResponse(template, context)
 
 @router.put("/contact/1", response_class=HTMLResponse)
 async def put_contact(request: Request, hx_request: Optional[str] = Header(None)):
