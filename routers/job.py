2b88c22ff95b90c62cf464bfc4adda86db35129e
luke-1993 <luke_purnell@hotmail.com>
2023-11-14 09:01:08 +0000

    * a hacky but working PoC showing the job progress bar returning an event that then triggers the job.

--- routers/job.py
+++ routers/job.py
@@ -9,15 +9,15 @@ i = 0
 @router.get("/job/progress", response_class=HTMLResponse)
 async def job_progress(request: Request, hx_request: Optional[str] = Header(None)):
     global i
+    context = {"request": request, 'value': i}
     templates = Jinja2Templates(directory="ui/templates")
     print('progress')
     print(i)
 
 
-    context = {"request": request, 'value': i}
     i += 20
     if hx_request:
         if i <= 100:
-            return templates.TemplateResponse("partials/job_progress.html", context)
-
-    return templates.TemplateResponse("home.html", context)
+            resp = templates.TemplateResponse("partials/job_progress.html", context)
+            resp.headers['HX-Trigger'] = 'start-job'
+            return resp
