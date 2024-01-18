8ba18df4ae808cadb76c76078837587016733cf4
luke-1993 <luke_purnell@hotmail.com>
2024-01-18 15:18:44 +0000

    * add logic for serato import and export. TODO: test

--- subbox_landing/routers/djs.py
+++ subbox_landing/routers/djs.py
@@ -130,3 +130,82 @@ async def export_rekordbox(
         }
         template = templates.TemplateResponse("partials/generic_failure.html", context)
     return template
+
+@router.post("/djs/export/serato", response_class=HTMLResponse)
+@inject
+async def export_serato(
+        request: Request,
+        session_id: str | None = Cookie(None),
+        local_root: str = Form(...),
+        session: ClientSession = Depends(Provide[Container.aiohttp_session])
+):
+    print(local_root)
+    templates = Jinja2Templates(directory="subbox_landing/ui/templates")
+
+    success = False
+    context = {"request": request}
+    status_code = None
+    response_json = ""
+    if session_id or isinstance(session, mock.AsyncMock):
+        data = {
+            'session_id': session_id,
+            'user_root': local_root
+        }
+        async with session.post('http://pymix:8002/serato/export', params=data) as response:
+            status_code = response.status
+            if response.status == HTTPStatus.OK:
+                response_json = await response.json()
+                print(response_json)
+                success = True
+                n_beets_tracks = response_json['n_beets_tracks']
+                context['n_beets_tracks'] = n_beets_tracks
+
+    if success:
+        template = templates.TemplateResponse("partials/serato_export_results.html", context)
+    else:
+        context["error"] = {
+            'status_code': status_code,
+            'response': response_json,
+            'message': f'Failed to complete export job for session id {session_id}'
+        }
+        template = templates.TemplateResponse("partials/generic_failure.html", context)
+    return template
+
+@router.post("/djs/upload/serato", response_class=HTMLResponse)
+@inject
+async def upload_serato(
+        request: Request,
+        session_id: str | None = Cookie(None),
+        hx_request: Optional[str] = Header(None),
+        session: ClientSession = Depends(Provide[Container.aiohttp_session])
+):
+    templates = Jinja2Templates(directory="subbox_landing/ui/templates")
+
+    success = False
+    context = {"request": request}
+    status_code = None
+    response_json = ""
+    if session_id or isinstance(session, mock.AsyncMock):
+        data = {
+            'session_id': session_id
+        }
+        async with session.post('http://pymix:8002/serato/import', params=data) as response:
+            status_code = response.status
+            if response.status == HTTPStatus.OK:
+                response_json = await response.json()
+                success = True
+                total_n_imported_tracks = response_json['imported_tracks']
+                beets_output = response_json['beets_output']
+                context['total_n_imported_tracks'] = total_n_imported_tracks
+                context['beets_output'] = beets_output
+
+    if success:
+        template = templates.TemplateResponse("partials/job_results.html", context)
+    else:
+        context["error"] = {
+            'status_code': status_code,
+            'response': response_json,
+            'message': f'Failed to complete import job for session id {session_id}'
+        }
+        template = templates.TemplateResponse("partials/generic_failure.html", context)
+    return template
